278f56bb82dbfa7ac3efbcd5fa3f2f3f38c80fa5
einarf <eforselv@gmail.com>
2020-09-28 10:07:43 +0200

    Remove Vagrant setup

--- vm_provision/provision-ubuntu-14.04.sh
+++
@@ -1,8 +0,0 @@
-
-# Some useful packages..
-apt-get install -y libpython2.7-dev
-apt-get install -y python-pip
-apt-get install -y python-virtualenv
-apt-get install -y libffi-dev
-apt-get install -y libssl-dev
-apt-get install -y git
